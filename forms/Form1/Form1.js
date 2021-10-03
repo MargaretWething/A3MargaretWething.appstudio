2788a767db32a7c5489ac9f7c1e0863126e29020
MargaretWething <mfw86112@creighton.edu>
2021-10-03 15:48:29 -0500

    made cities output

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,19 @@
+let addCity = true
+let cities = []
+while (addCity) {
+  if (cities == ""){
+  userCity = prompt('Enter a city: ')
+  cities.push(userCity)
+  addCity = confirm('Too add another city, hit Ok, if not, hit Cancel.')
+  }
+else {
+  userCity = prompt('Enter another city: ')
+  cities.push(userCity)
+  addCity = confirm('Too add another city, hit Ok, if not, hit Cancel.')
+  }
+}
+let i = 0 
+while (i < cities.length) {
+  console.log(cities[i].toLowerCase())
+  i++;
+  }
